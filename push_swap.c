62680aa206bfe30c77db94e3e575ab6bd3808045
Ismail Lahyani <ilahyani@e2r11p2.1337.ma>
2022-01-03 01:49:45 +0100

    update

--- push_swap.c
+++ push_swap.c
@@ -6,26 +6,43 @@
 /*   By: ilahyani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 21:03:02 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/01/02 01:33:27 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/01/02 23:56:09 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <stdio.h> 
 
+int	isSorted(t_list *a)
+{
+	t_list *tmp;
+
+	tmp = a;
+	while (tmp->next && tmp->content < tmp->next->content)
+	{
+		printf("%d < %d\n", tmp->content, tmp->next->content);
+		tmp = tmp->next;
+	}
+	if(!(tmp->next))
+		return (1);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
-	int	i;
+	int		i;
+	void	*x;
 	t_list *a;
 
 	i = 1;
 	while (i < argc)
-		ft_lstadd_back(&a, ft_lstnew(argv[i++]));
-	i = 0;
-	while (a)
 	{
-		printf("%s\n", a->content);
-		a = a->next;
+		x = (void *)ft_atoi(argv[i++]);
+		ft_lstadd_back(&a, ft_lstnew(x));
 	}
+	if (isSorted(a))
+		ft_printf("sorted\n");
+	else
+		ft_printf("sortedn't\n");
 	return (0);
 }
