de0aed5ad9614c3680bcc06577f5cfe88ff91f7d
Ismail Lahyani <ilahyani@e2r5p4.1337.ma>
2022-01-06 11:02:42 +0100

    update

--- push_swap.c
+++ push_swap.c
@@ -6,46 +6,82 @@
 /*   By: ilahyani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 21:03:02 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/01/03 01:52:00 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/01/06 11:02:29 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <stdio.h> 
 
-int	isSorted(t_list *a)
+int	isSorted(int *A, int c)
 {
-	t_list *tmp;
+	int	i;
+	
+	i = 0;
+	while (i < c - 2 && A[i] <= A[i + 1])
+		i++;
+	if (i == c - 2)
+		return (1);
+	return (0);
+}
 
-	tmp = a;
-	while (tmp->next && tmp->content < tmp->next->content)
+void	sort_three(int *A)
+{
+	if (A[0] > A[1] && A[0] < A[2])
+			ft_printf("ra\n");
+	else if (A[1] > A[0] && A[1] > A[2])
 	{
-		ft_printf("here\n");
-		tmp = tmp->next;
+		if (A[0] < A[2])
+			ft_printf("rra\nsa\n");
+		else
+			ft_printf("rra");
 	}
-	if(tmp->next)
-		return (0);
-	return (1);
+	else if (A[0] > A[1] && A[0] > A[2])
+	{
+		if (A[1] > A[2])
+			ft_printf("sa\nrra\n");
+		else
+			ft_printf("ra\n");
+	}
+}
+void	sort_five(int *A)
+{
+	ft_printf("pb\npb\n");
+	sort_three(A + 2);
 }
 
 int	main(int argc, char **argv)
 {
 	int	i;
-	t_list *a;
-	int	x;
+	int	*A = NULL;
 
 	i = 1;
-	a = NULL;
 	if (argc == 1)
+		return (0);
+	A = malloc (sizeof(int) * argc - 1);
+	if (!A)
 		return (0);
 	while (i < argc)
 	{
-		x = ft_atoi(argv[i++]);
-		ft_lstadd_back(&a, ft_lstnew(&x));
+		A[i - 1] = ft_atoi(argv[i]);
+		i++;
 	}
-	if (!isSorted(a))
-		ft_printf("sortedn't\n");
-	else
-		ft_printf("sorted\n");
+	if (isSorted(A, argc))
+	{
+		free(A);
+		return (0);
+	}
+	else if (argc == 4)
+	{
+		sort_three(A);
+		free(A);
+		return (0);
+	}
+	else if (argc == 6)
+	{
+		sort_five(A);
+		free(A);
+		return (0);
+	}
 	return (0);
 }
