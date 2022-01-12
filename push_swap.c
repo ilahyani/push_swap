d4e7b644ca377775529bc325c2c82f5605337ecc
Ismail Lahyani <ilahyani@e2r3p11.1337.ma>
2022-01-12 21:24:45 +0100

    dozen_sort done

--- push_swap.c
+++ push_swap.c
@@ -13,8 +13,8 @@
 #include "libft/libft.h"
 #include <stdio.h> 
 
-//This gotta be on a utils file
 /////////////////////////////////////////////////////////////////////////////////////////////////
+
 void	sa(int *A, int A_top)
 {
 	int	tmp;
@@ -44,8 +44,8 @@ void	ss(int *A, int A_top, int *B, int B_top)
 
 void	pa(int *A, int *B, int *B_top, int *A_top) 
 {
-	(*A_top)++;				// Handle the stack overflow/underflow case (if (B_top++ > B_size) || (B_top-- < 0) then exit(1))
-	*(A + *B_top) = *(B + *A_top);
+	(*A_top)++; // Handle the stack overflow/underflow case (if (B_top++ > B_size) || (B_top-- < 0) then exit(1))
+	*(A + *A_top) = *(B + *B_top);
 	(*B_top)--;
 	ft_printf("pa\n");
 }
@@ -53,7 +53,7 @@ void	pa(int *A, int *B, int *B_top, int *A_top)
 void	pb(int *A, int *B, int *B_top, int *A_top)
 {
 	(*B_top)++;
-	*(B + *A_top) = *(A + *B_top);
+	*(B + *B_top) = *(A + *A_top);
 	(*A_top)--;
 	ft_printf("pb\n");
 }
@@ -64,8 +64,8 @@ void	ra(int *A, int A_top)
 
 	while (A_top > 0)
 	{
-		tmp = *(A + A_top);
-		*(A + A_top) = *(A + A_top);
+		tmp = *(A + A_top - 1);
+		*(A + A_top -1) = *(A + A_top );
 		*(A + A_top) = tmp;
 		A_top--;
 	}	
@@ -78,9 +78,9 @@ void	rb(int *B, int B_top)
 
 	while (B_top > 0)
 	{
-		tmp = *(B + B_top);
-		*(B + B_top) = *(B + B_top);
-		*(B+ B_top) = tmp;
+		tmp = *(B + B_top - 1);
+		*(B + B_top - 1) = *(B + B_top);
+		*(B + B_top) = tmp;
 		B_top--;
 	}
 	ft_printf("rb\n");
@@ -167,26 +167,37 @@ void	sort_three(int *A, int A_top)
 	}
 }	
 
-void	smallest_to_top(int *A, int A_top)
+void	smallest_to_top(int *A, int *A_top)
 {
 	int	i;
 	int	idx;
 
-	i = 1;
-	idx = 0;
-	while (i < A_top)
+	i = 0;
+	idx = 1;
+	while (i <= *A_top)
 	{
 		if (A[idx] > A[i])
 			idx = i;
 		i++;
 	}
-	ft_printf("smallest A[%d] = %d\n", idx, A[idx]);
-	if (idx > A_top / 2)	
-		while (idx++ <= A_top)
-			rra(A, A_top);
-	else			
-		while (idx-- > 0)
-			ra(A, A_top);
+	if (idx == *A_top)
+		return ;
+	if (idx < (*A_top) / 2)
+	{
+		while (idx >= 0)
+		{
+			rra(A, *A_top);
+			idx--;
+		}
+	}
+	else
+	{	
+		while (idx < *A_top)
+		{
+			ra(A, *A_top);
+			idx++;
+		}
+	}
 }
 
 void	dozen_sort(int *A, int *A_top, int *B, int *B_top)
@@ -194,16 +205,44 @@ void	dozen_sort(int *A, int *A_top, int *B, int *B_top)
 	int	count;
 	int	i;
 
-	count = *A_top;
+	count = *A_top + 1;
 	i = count;
-	while (count > 0)
+	while (count > 3)
 	{
-		smallest_to_top(A, *A_top);
+		if (isSorted(A, *A_top))
+			break ;
+		//printf("B4 smallest to top -------------------------------------\n");
+		//for (int k = *A_top; k >= 0; k--)
+		//	ft_printf("A[%d] = %d | ", k, A[k]);
+		//printf("\n");
+		smallest_to_top(A, A_top);
+		// printf("A_top before pb -> %d\n", *A_top);
+		// printf("B_top before pb -> %d\n", *B_top);
+		//printf("after smallest to top-----------------------------------\n");
+		//for (int k = *A_top; k >= 0; k--)
+		//	ft_printf("A[%d] = %d | ", k, A[k]);
+		//printf("\n");
 		pb(A, B, B_top, A_top);
+		//printf("after pb------------------------------------------------\n");
+		//for (int b = *B_top; b >= 0; b--)
+		//	ft_printf("B[%d] = %d | ", b, B[b]);
+		//printf("\n");
+		//printf("--------------------------------------------------------\n");
+		// printf("A_top after pb  -> %d\n", *A_top);
+		// printf("B_top after pb  -> %d\n", *B_top);
 		count--;
 	}
-	while (count++ < i)
+	if (!isSorted(A, *A_top))
+		sort_three(A, *A_top);
+	while (count < i)
+	{
+		// printf("A_top before pa -> %d\n", *A_top);
+		// printf("B_top before pa -> %d\n", *B_top);
 		pa(A, B, B_top, A_top);
+		// printf("A_top after pa  -> %d\n", *A_top);
+		// printf("B_top after pa  -> %d\n", *B_top);
+		count++;
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -222,16 +261,14 @@ int	main(int argc, char **argv)
 	B = malloc (sizeof(int) * argc - 1);
 	if (!A)
 		return (0);
-	i = 1;
-	while (i < argc)
-	{
-		A[i - 1] = ft_atoi(argv[i]);	//Need an error msg if input ain't int
-		i++;
-	}
-//	for (i = 0; i < argc - 1; i++)
-//		ft_printf("A[%d] = %d\n", i, A[i]);
 	A_top = argc - 2;
 	B_top = -1;
+	argc -= 1;
+	i = 0;
+	while (i <= A_top)
+		A[i++] = ft_atoi(argv[argc--]);	//Need an error msg if input ain't int
+	for (i = A_top; i >= 0; i--)
+		ft_printf("A[%d] = %d\n", i, A[i]);
 	if (isSorted(A, A_top))
 	{
 		ft_printf("already sorted");
@@ -241,17 +278,13 @@ int	main(int argc, char **argv)
 	}
 	else if (argc == 4)
 		sort_three(A, A_top);
-	else if (argc <=  10)
+	else if (argc <  12)
 		dozen_sort(A, &A_top, B, &B_top);
-	ft_printf("[%d]\n", A_top);
-	while (A_top >= 0)
-	{
-		ft_printf("A[%d] = %d\n", A_top, A[A_top]);
-		A_top--;
-	}
+	// ft_printf("[%d]\n", A_top);
+	// ft_printf("[%d]\n", B_top);
+	for (i = A_top; i >= 0; i--)
+		ft_printf("A[%d] = %d\n", i, A[i]);
 	free(A);
 	free(B);
 	return (0);
 }
-
-//TODO: debug sort_dozen 
